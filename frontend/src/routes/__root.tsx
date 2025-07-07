e51fa9e6327eb686fa921fc6971690551cdf5082
Sygix <17769298+Sygix@users.noreply.github.com>
2025-07-08 00:55:37 +0200

    style(layout): enhance styling for root and index routes

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import bg from '../assets/images/w11.png'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <main className='text-carbon-900 relative flex min-h-screen flex-col'>
+      <img src={bg} alt="background image" className='h-full w-full absolute -z-50 max-h-full object-cover'/>
       <div className="p-2 flex gap-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
@@ -12,9 +14,8 @@ export const Route = createRootRoute({
           About
         </Link>
       </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </main>
   ),
 })
